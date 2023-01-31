ce0645ac0ec28b7486839bb625c625c3254e2d97
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2023-01-31 23:35:14 +0900

    関連動画の取得まで

---
+++ src/apis/index.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const KEY='AIzaSyDfUK4LsRrBLI8kaEKyhO4Gv3bklcA6PyE'
+
+const youtube = axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3'
+})
+
+const params = {
+  part: 'snippet',
+      maxResult: 40,
+      key: KEY,
+      regionCode: 'JP',
+      type: 'video'
+}
+export const fetchPopularData=async()=>{
+  return await youtube.get('/videos', {
+    params: {
+      ...params,
+      chart: 'mostPopular'
+    }
+  })
+}
+
+export const fetchSelectedData= async(id)=>{
+  return await youtube.get('videos', {
+  params: {
+    ...params,
+    id
+  }
+  })
+}
+
+export const fetchRelatedData = async (id) =>{
+  return await youtube.get('/search', {
+    params: {
+      ...params,
+      relatedToVideoId: id
+    }
+  })
+}
---
+++ src/components/SideList/SideList.js
@@ -0,0 +1,37 @@
+import React, { useContext, useEffect } from 'react'
+import { fetchRelatedData } from '../../apis'
+import { Store } from '../../store'
+import SideListItem from '../SideListItem/SideListItem'
+import Style from './SideList.module.scss'
+
+const SideList = () => {
+  const {globalState, setGlobalState }=useContext(Store)
+  const setRelatedVideo = async (id) =>{
+    await fetchRelatedData(id).then((res)=>{
+      setGlobalState({type: 'SET_RELATED', payload: {related: res.data.items}})
+    })
+  }
+  useEffect(()=>{
+    setRelatedVideo(globalState.selected.id)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[globalState.selected])
+  return (
+    <div className={Style.sidenav}>
+      {
+        globalState.related ? globalState.related.map((video)=>{
+          return(
+            <SideListItem
+              id={video.id.videoId}
+              key={video.id.videoId}
+              src={video.snippet.thumbnails.medium.url}
+              title={video.snippet.title}
+            />
+          )
+        }) : <span>no data</span>
+      }
+      
+    </div>
+  )
+}
+
+export default SideList
